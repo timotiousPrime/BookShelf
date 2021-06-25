634820a14c218a56df40ee1d6ccdc964ff984382
Timo <timodb031@gmail.com>
2021-06-25 10:15:46 +0200

    Create bookcard function

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ function Book(title, author, pages, pagesRead, rating) {
     this.rating = rating;
 };
 
-// function displayBooks () {
-//     let myBooks = myLibrary.map( function (book) {
-//         console.log(book.title);
-//         console.log(book.author);
-//         console.log(book.pages);
-//         console.log(book.pagesRead);
-//         console.log(book.rating);
-//     })
-// }
+function displayBooks () {
+    let myBooks = myLibrary.map( function (book) {
+        console.log(book.title);
+        console.log(book.author);
+        console.log(book.pages);
+        console.log(book.pagesRead);
+        console.log(book.rating);
+    })
+}
 
 // function createNewBook(title, author, pages, pagesRead, rating) {
 //     let newBook = new Book (title, author, pages, pagesRead, rating)
@@ -29,6 +29,47 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const pagesRead = document.querySelector('#pagesRead');
 const rating = document.querySelector('#rating');
+// 
+
+// Create page entry for book
+const bookDiv = document.createElement('div');
+const bookTitle = document.createElement('p');
+const bookAuthor = document.createElement('p');
+const bookPages = document.createElement('p');
+const bookPagesRead = document.createElement('p');
+const bookComplete = document.createElement('p');
+const bookRating = document.createElement('p');
+const bookShelf = document.querySelector('#bookShelf');
+
+function createBookCard (title,  author, pages, pagesRead, rating) {
+    bookShelf.appendChild(bookDiv)
+    bookDiv.classList.add('book');
+
+    bookDiv.appendChild(bookTitle);
+    bookTitle.classList.add('title');
+    bookTitle.textContent = title;
+
+    bookDiv.appendChild(bookAuthor);
+    bookAuthor.classList.add('author');
+    bookAuthor.textContent = author;
+
+    bookDiv.appendChild(bookPages);
+    bookPages.classList.add('pages');
+    bookPages.textContent = pages;
+
+    bookDiv.appendChild(bookPagesRead);
+    bookPagesRead.classList.add('pagesRead');
+    bookPagesRead.textContent = pagesRead;
+
+    // bookDiv.appendChild(bookComplete);
+    // bookComplete.classList.add('complete');
+    // bookComplete.textContent = title;
+
+    bookDiv.appendChild(bookRating);
+    bookRating.classList.add('rating');
+    bookRating.textContent = rating;
+
+}
 // 
 
 function addBookToLibrary() {
