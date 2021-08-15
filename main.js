465db3f145a8da79b842334641669bbac99a2362
timotiousPrime <timodb031@gmail.com>
2021-08-15 22:46:30 +0200

    books display as accordian. must make bk btns work

--- main.js
+++ main.js
@@ -1,91 +1,103 @@
 let myLibrary = {}
 
-function BOOK (title, author, pages, pagesRead, completed, rating, id) {
+function BOOK (title, author, pages, pagesRead, completed, rating, summary, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.pagesRead = pagesRead
     this.completed = completed
     this.rating = rating
+    this.summary = summary
     this.id = id
 }
 
+let newBookCard = (id, title, author, pages, pagesRead, complete, rating, summary) => {
+    let target = `collapseBook${id}`
+    let targetHeading = `heading${id}`
+    
+    let template = `
+    <div class="accordian-item book">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-${target}" aria-expanded="false" aria-controls="flush-${target}">
+            <h4 class="accordian-header h3" data-toggle="collapse" href="#collapse1">
+                    <p class="h3">${title}</p>
+                    <p class="h6">${author}</p>
+                </h4>
+            </button>
+            </div>
+        <div id="flush-${target}" class="accordion-collapse collapse" aria-labelledby="flush-${targetHeading}" data-bs-parent="#flush-${target}">
+        <div class=" row">
+                <div class="col-sm-3">Pages:</div>
+                <div class="col-sm">${pages}</div>
+                </div>
+            <div class=" row">
+            <div class="col-sm-3">Pages read:</div>
+                <div class="col-sm">${pagesRead}</div>
+                </div>
+                <div class=" row">
+                <div class="col-sm-3">Rating:</div>
+                <div class="col-sm">${rating}</div>
+                </div>
+                <div class=" row">
+                <div class="col-sm-3">completed:</div>
+                <div class="col-sm">${complete}</div>
+                </div>
+                <div class=" col">
+                <div class="col-sm-3">Summary:</div>
+                <div class="col-sm">${summary}</div>
+                </div>
+                <div container-fluid row>
+                <button type="button" id="completeBtn" class="btn btn-success">Complete</button>
+                <button type="button" id="editBtn" class="btn btn-warning">Edit</button>
+                <button type="button" id="deleteBtn" class="btn btn-danger">Delete</button>
+                </div>
+                </div>
+    `
+    
+    const accordian = document.createElement('div')
+    accordian.classList.add('accordian', 'accordian-flush', 'book')
+    accordian.innerHTML = template
+    accordian.setAttribute('id', `${id}`)
+    
+    bookCase.appendChild(accordian)
+}
+
+// User inputs from form
 const titleInput = document.getElementById('title-input')
 const authorInput = document.getElementById('author-input')
 const pagesInput = document.getElementById('pages-input')
 const pagesReadInput = document.getElementById('pages-read-input')
 const completedInput = document.getElementById('completed-input')
 const ratingInput = document.getElementById('rating-input')
+const summaryInput = document.getElementById('summary-input')
 const form = document.getElementById('form')
 const bookCase = document.getElementById('book-case')
 
-
-
-
-let newBookCard = (id, title, author, pages, pagesRead, complete, rating) => {
-// define elements to create for book card
-const tableRow = document.createElement('tr')
-const bookTitle = document.createElement('td')
-const bookAuthor = document.createElement('td')
-const bookPages = document.createElement('td')
-const bookPagesRead = document.createElement('td')
-const bookComplete = document.createElement('td')
-const bookRating = document.createElement('td')
-
-    bookCase.appendChild(tableRow)
-    tableRow.setAttribute('id', id)
-    tableRow.classList.add('book')
-
-    tableRow.appendChild(bookTitle)
-    bookTitle.classList.add('title')
-    bookTitle.textContent = title
-
-    tableRow.appendChild(bookAuthor)
-    bookAuthor.classList.add('author')
-    bookAuthor.textContent = author
-
-    tableRow.appendChild(bookPages)
-    bookPages.classList.add('pages')
-    bookPages.textContent = pages
-
-    tableRow.appendChild(bookPagesRead)
-    bookPagesRead.classList.add('pagesRead')
-    bookPagesRead.textContent = pagesRead
-
-    tableRow.appendChild(bookComplete)
-    bookComplete.classList.add('complete')
-    bookComplete.textContent = complete
-
-    tableRow.appendChild(bookRating)
-    bookRating.classList.add('rating')
-    bookRating.textContent = rating
-
-}
-
-let bookId = 0
-
+// Creates a new book using the info from the form inputs
 const createNewBook = () => {
-    bookId += 1
-
+    let bookId = getNextBookId(myLibrary)
+    
     completedInput.checked ? pagesReadInput.value = pagesInput.value : console.log('finish the damn book')
+    
+    return new BOOK (titleInput.value, authorInput.value, pagesInput.value, pagesReadInput.value, completedInput.checked, ratingInput.value, summaryInput.value, bookId)
+}    
 
-    return new BOOK (titleInput.value, authorInput.value, pagesInput.value, pagesReadInput.value, completedInput.checked, ratingInput.value, bookId)
-}
-
+// check for saved library in LocalStorage
 window.addEventListener('load', () => {
     console.log('page has loaded')
-    !localStorage.theLibrary ? console.log('there are no books saved yet') : updateMyLibrary(), displayMyLibrary(), console.log(myLibrary) 
+    !localStorage.theLibrary ? console.log('there are no books saved yet') : updateMyLibrary(), displayMyLibrary(), console.log('myLibrary on window load'), console.log(myLibrary) 
 })
 
-const updateMyLibrary = () => {
 
+const updateMyLibrary = () => {
     myLibrary = unpackTheLibrary()
+}
+
+const getNextBookId = (library) => {
+    return Object.keys(library).length +1
 }
 
 const unpackTheLibrary = () => {
     const library = JSON.parse(localStorage.theLibrary)
-    console.log(library)
-    console.log(myLibrary)
     return library
 }
 
@@ -111,14 +123,12 @@ function removeCard (item) {
 
 const displayMyLibrary = () => {
     let keys = Object.keys(myLibrary)
-
-    console.log(keys)
     
     const newBookCards = document.querySelectorAll('.book');
     newBookCards.forEach(removeCard);
 
     keys.forEach( (key) => {
-        newBookCard(myLibrary[key].id, myLibrary[key].title, myLibrary[key].author, myLibrary[key].pages, myLibrary[key].pagesRead, myLibrary[key].completed, myLibrary[key].rating)
+        newBookCard(myLibrary[key].id, myLibrary[key].title, myLibrary[key].author, myLibrary[key].pages, myLibrary[key].pagesRead, myLibrary[key].completed, myLibrary[key].rating, myLibrary[key].summary)
     })
 }
 
@@ -127,25 +137,27 @@ const handleAddBookClick = () => {
     form.reset()
     saveLibrary(myLibrary)
     displayMyLibrary()
-    listenForMouseEventsOnBooks()
+    // listenForMouseEventsOnBooks()
 }
 
 const addBookButton = document.getElementById('addBookButton')
 addBookButton.addEventListener('click', handleAddBookClick)
 
-listenForMouseEventsOnBooks()
+// listenForMouseEventsOnBooks()
 
-function mouseOver () {
-    this.classList.add('bg-info')
-}
+// function mouseOver () {
+//     console.log(this)
+//     this.classList.add('bg-info')
+// }
 
-function mouseOut () {
-    this.classList.remove('bg-info')
-}
+// function mouseOut () {
+//     console.log('bye')
+//     this.classList.remove('bg-info')
+// }
 
-function showBook (book) {
-    openBookDetails(book)
-}
+// function showBook (book) {
+//     openBookDetails(book)
+// }
 
 function listenForMouseEventsOnBooks() {
 
@@ -161,117 +173,20 @@ function listenForMouseEventsOnBooks() {
     })
 }
 
-function openBookDetails(book) {
-    console.log(book)
-    // console.log(`Book ${book.id} is open`)
-//     console.log(book)
+listenForMouseEventsOnAccordianBtns()
 
-    
-//     const bookDetailsOverlay = document.createElement('div')
-//     const bookDetailCard = document.createElement('div')
+function listenForMouseEventsOnAccordianBtns() {
 
-//     const bookTitleHeading = document.createElement('h2')
-//     const bookAuthorHeading = document.createElement('h4')
-
-//     const bookDetailsNameDiv = document.createElement('div')
-//     const bookDetailsDiv =  document.createElement('div')
-
-//     const bookPages = document.createElement('label')
-//     const bookPagesRead = document.createElement('label')
-//     const bookRating = document.createElement('label')
-//     const bookSummary = document.createElement('label')
-
-//     const btnDiv = document.createElement('div')
-//     const completedBtn = document.createElement('button')
-//     const editdBtn = document.createElement('button')
-//     const deletedBtn = document.createElement('button')
-
-//     // const bookPagesValue = document.createElement('label')
-//     // const bookPagesReadValue = document.createElement('label')
-//     // const bookRatingValue = document.createElement('label')
-//     // const bookSummaryValue = document.createElement('label')
-
-//     const bookPagesValueInput = document.createElement('input')
-//     const bookPagesReadValueInput = document.createElement('input')
-//     const bookRatingValueInput = document.createElement('input')
-//     const bookSummaryValueInput = document.createElement('input')
-
-//     document.body.appendChild(bookDetailsOverlay)
-//     bookDetailsOverlay.setAttribute('id', 'bookDetailsOverlay')
-//     bookDetailsOverlay.appendChild(bookDetailCard)
-//     bookDetailCard.setAttribute('id', 'bookDetailCard')
-//     bookDetailCard.classList.add('container')
-
-//     bookDetailCard.appendChild(bookTitleHeading)
-//     bookTitleHeading.textContent = `${book.title}`
-//     bookTitleHeading.classList.add('row')
-//     bookDetailCard.appendChild(bookAuthorHeading)
-//     bookAuthorHeading.textContent = `${book.author}`
-//     bookAuthorHeading.classList.add('row')
-
-//     bookDetailCard.appendChild(bookDetailsNameDiv)
-//     bookDetailsNameDiv.classList.add('col')
-//     bookDetailsNameDiv.appendChild(bookPages)    
-//     bookPages.textContent = `Pages:`
-//     bookPages.classList.add('row')
-//     bookDetailCard.appendChild(bookPagesRead)
-//     bookPagesRead.classList.add('row')
-//     bookDetailCard.appendChild(bookRating)
-//     bookRating.classList.add('row')
-//     bookDetailCard.appendChild(bookSummary)
-//     bookSummary.classList.add('row')
-
-//     bookDetailCard.appendChild(bookDetailsDiv)
-//     bookDetailsDiv.classList.add('col')
-//     bookPages.appendChild(bookPages)
-//     bookPagesValue.textContent = `Pages:`
-//     console.log(`${book.pages}`)
-//     bookPagesValue.classList.add('row')
-//     bookDetailCard.appendChild(bookPagesReadValue)
-//     bookPagesReadValue.textContent = `Pages read:`
-//     console.log(`${book.pagesRead}`)
-//     bookPagesReadValue.classList.add('row')
-//     bookDetailCard.appendChild(bookRatingValue)
-//     bookRatingValue.textContent = `Book Rating:`
-//     console.log(`${book.rating}`)
-//     bookRatingValue.classList.add('row')
-//     bookDetailCard.appendChild(bookSummaryValue)
-//     bookSummaryValue.textContent = `This is a summary`
-//     console.log(`${book.title}`)
-//     bookSummaryValue.classList.add('row')
-
-
-//     // Inputs
-//     // bookDetailCard.appendChild(bookDetailsDiv)
-//     // bookDetailsDiv.classList.add('col')
-//     bookDetailCard.appendChild(bookPagesValueInput)
-//     bookPages.classList.add('row')
-//     bookDetailCard.appendChild(bookPagesReadValueInput)
-//     bookPagesReadValue.classList.add('row')
-//     bookDetailCard.appendChild(bookRatingValueInput)
-//     bookRatingValue.classList.add('row')
-//     bookDetailCard.appendChild(bookSummaryValueInput)
-//     bookSummaryValue.classList.add('row')
-
-
-//     // Btns
-//     bookDetailCard.appendChild(btnDiv)
-//     btnDiv.setAttribute('id', 'btnDiv')
-//     btnDiv.classList.add('row')
-//     btnDiv.appendChild(completedBtn)
-//     completedBtn.textContent = 'Book Complete'
-//     completedBtn.classList.add('btn', 'col', 'btn-success')
-//     btnDiv.appendChild(editdBtn)
-//     editdBtn.textContent = 'Edit Details'
-//     editdBtn.classList.add('btn', 'col', 'btn-secondary')
-//     btnDiv.appendChild(deletedBtn)
-//     deletedBtn.textContent = 'Delete Book'
-//     deletedBtn.classList.add('btn', 'col', 'btn-danger')
-
+    const accordianEls = document.querySelectorAll('.book')
+    accordianEls.forEach( accordian => {
+        accordian.addEventListener('click', (e) => {
+            console.log(e)
+        })
+    })
 }
 
-function createBookDetailsCard() {
-
+const logBtn = () => {
+    console.log(e)
 }
 
 function listenForEditClick() {
