63b2ad178d9783470c4aa3a378fabcd296be7756
Timo <timodb031@gmail.com>
2021-12-07 16:12:32 +0200

    validate new books AND updating books

--- formValidation.js
+++ formValidation.js
@@ -1,13 +1,17 @@
 import { BUTTONS } from "./constants.js"
 
-export function validateBookEntry () {
+export function validateBookEntry (isNewEntry) {
     if (USER_INPUTS.title.value === '' ||
         USER_INPUTS.author.value === '' || 
         USER_INPUTS.pages.value < 0 || 
         USER_INPUTS.pagesRead.value < 0) {
             console.log('invalid')
-            disableAddBookBtn()    
-        
+            if (isNewEntry) {
+                disableAddBookBtn()
+            }
+            else {
+                disableupdateBtn()   
+            }
         }
 }    
 
@@ -15,3 +19,7 @@ export function validateBookEntry () {
 function disableAddBookBtn () {
     BUTTONS.addBook.setAttribute('disabled')
 }
+
+function disableupdateBtn () {
+    BUTTONS.update.setAttribute('disabled')
+}    
