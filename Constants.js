180bd86c2e4759e18b005c69aa43e56abf0e2acb
Timo <timodb031@gmail.com>
2021-12-07 09:13:22 +0200

    created constants

---
+++ Constants.js
@@ -0,0 +1,13 @@
+// User inputs from form
+const USER_INPUTS = {
+    title: document.getElementById('title-input'),
+    author: document.getElementById('author-input'),
+    pages: document.getElementById('pages-input'),
+    pagesRead: document.getElementById('pages-read-input'),
+    completed: document.getElementById('completed-input'),
+    rating: document.getElementById('rating-input'),
+    summary: document.getElementById('summary-input'),
+    form: document.getElementById('form'),
+    bookCase: document.getElementById('book-case'),
+}
+
