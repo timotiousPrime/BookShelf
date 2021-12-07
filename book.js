efb16128b7943528c42f4c4ed2952ddbdfcf4b9d
Timo <timodb031@gmail.com>
2021-12-07 08:53:02 +0200

    book class with methods

---
+++ book.js
@@ -0,0 +1,93 @@
+class Book {
+
+    static bookIdCounter = 0;
+    static generateID() {return ++Book.bookIdCounter;}
+
+    completed = false;
+    pages = 0;
+    pagesRead = 0;
+    rating = 3;
+    summary = '';
+    id = Book.generateID();
+
+    constructor(title, author, pages, pagesRead, completed, rating, summary) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.pagesRead = pagesRead;
+        this.completed = completed;
+        this.rating = rating;
+        this.summary = summary;
+    }
+
+    get title() {
+        return this.title;
+    }
+
+    setTitle(value) {
+        this.title = value;
+    }
+
+    get author() {
+        return this.author;
+    }
+
+    setAuthor(value) {
+        this.author = value;
+    }
+
+    get pages() {
+        return this.pages;
+    }
+
+    setPages(value) {
+        this.pages = value;
+    }
+
+    get pagesRead() {
+        return this.pagesRead;
+    }
+
+    setPagesRead(value) {
+        this.pagesRead = value;
+    }
+
+    get completed() {
+        return this.completed;
+    }
+
+    completed() {
+        this.completed = true;
+        this.pagesRead = this.pages;
+    }
+
+    notCompleted() {
+        this.completed = false;
+        this.pagesRead = 0;
+    }
+
+    get rating() {
+        return this.rating;
+    }
+
+    setRating(value) {
+        this.rating = value;
+    }
+
+    get summary() {
+        return this.summary;
+    }
+
+    setSummary(value) {
+        this.summary = value;
+    }
+
+    get id() {
+        return this.id;
+    }
+
+    setId(value) {
+        this.id = value;
+    }
+
+}
